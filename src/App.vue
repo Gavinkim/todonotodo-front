e49eb0fac80dc0dc7588b3081b3a1883b1e89c76
GavinKim <dreamworld612@gmail.com>
2018-08-17 01:35:39 +0900

    added nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <v-app>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"
+      class="hidden-sm-and-up">
+      </v-toolbar-side-icon>
+      <v-toolbar-title>TDNTD</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat><v-icon left>whatshot</v-icon>
+          What's Hot</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>whatshot</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>What's hot</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main></main>
   </v-app>
 </template>
 
@@ -9,7 +31,12 @@ export default {
   name: 'App',
   data () {
     return {
+        sideNav: false
     }
   }
 }
 </script>
+
+<style lang="stylus">
+  @import './stylus/main.styl'
+</style>
